6feab619767f48ca91af1217fd66a11b92334e4c
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-01-08 16:40:06 +0100

    changed auth request response to User

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 @Slf4j
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/authenticate")
-    public AuthenticationBean authenticate(Principal principal){
+    public User authenticate(Principal principal){
         log.info("authenticate");
         try{
-            return new AuthenticationBean(principal.getName());
+            log.info(principal.getName());
+            AuthenticationBean bean = new AuthenticationBean(principal.getName());
+            if(bean.getUsername() != null){
+                Optional<User> optionalUser = userRepository.findByUsername(principal.getName());
+                return optionalUser.get();
+            }else{
+                return null;
+            }
         }
         catch (Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to login");
